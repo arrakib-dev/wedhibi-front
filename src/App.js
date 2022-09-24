b8c240d311dfea11b2f74dd1324779362c9351b9
Abdur Rahman <abdur.rahman@tuhh.de>
2022-09-25 00:08:45 +0200

    optimize loading speed

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@
 
 // imports
 // ---------------------------------------------------
-
+import React, {lazy} from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
-import Home from './components/Home/Home'
-import DashboardLayout from './components/Dashboard/DashboardLayout'
-import Company from './components/Dashboard/Company/Company'
-import Member from './components/Dashboard/Member/Member'
-import Projects from './components/Projects/Projects'
-import Project from './components/Projects/Project/Project'
-import Tasks from './components/Tasks/Tasks'
-import Task from './components/Tasks/Task/Task'
-import Login from './components/Auth/UI/Login'
-import Signup from './components/Auth/UI/SIgnup'
-import Error from './components/Errors/Error'
+const Home = lazy(() => import('./components/Home/Home'))
+const DashboardLayout = lazy(() => import('./components/Dashboard/DashboardLayout'))
+const Company = lazy(() => import('./components/Dashboard/Company/Company'))
+const Member = lazy(() => import('./components/Dashboard/Member/Member'))
+const Projects = lazy(() => import('./components/Projects/Projects'))
+const Project = lazy(() => import('./components/Projects/Project/Project'))
+const Tasks = lazy(() => import('./components/Tasks/Tasks'))
+const Task = lazy(() => import('./components/Tasks/Task/Task'))
+const Login = lazy(() => import('./components/Auth/UI/Login'))
+const Signup = lazy(() => import('./components/Auth/UI/Signup'))
+const Error = lazy(() => import('./components/Errors/Error'))
 
 // Component codes
 // ---------------------------------------------------
